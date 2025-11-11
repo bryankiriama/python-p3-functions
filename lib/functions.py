1ee3682c02a9aeafe67165e985122ca81db7ae36
bryankiriama <bryankiriama@gmail.com>
2025-11-11 22:52:36 +0300

    difined fuctions with parameters

--- lib/functions.py
+++ lib/functions.py
@@ -2,13 +2,14 @@
 
 def greet_programmer():
     pass
-
+    print(f"Hello, programmer!")
 def greet(name):
     pass
+    print(f"Hello, {name}!")
 
 def greet_with_default(name="programmer"):
     pass
-
+    print(f"Hello, {name}!")
 def add(num1, num2):
     pass
 
